59ff0ff6eb3c5c572af70a227bff4c4f57b75c06
Maxime Henrion <mhenrion@gmail.com>
2025-07-19 21:09:47 -0400

    Implement the AsSlice trait for convenience.
    The AsVector trait is actually not what we want since it would destroy
    peformance: we really want to use slices for our keys since it makes
    recursion on suffixes very cheap, with no need for new allocations or
    copies.

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,34 @@
 /// every invariant.
 use replace_with::*;
 
-pub trait AsVector<K> {
-    fn as_vec(&self) -> Vec<K>;
+pub trait AsSlice<K> {
+    fn as_slice(&self) -> &[K];
 }
 
-impl AsVector<char> for str {
-    fn as_vec(&self) -> Vec<char> {
-        self.chars().collect()
+impl AsSlice<u8> for &str {
+    fn as_slice(&self) -> &[u8] {
+        self.as_bytes()
     }
 }
 
-impl AsVector<u8> for str {
-    fn as_vec(&self) -> Vec<u8> {
-        self.as_bytes().to_vec()
+impl AsSlice<u8> for String {
+    fn as_slice(&self) -> &[u8] {
+        self.as_bytes()
     }
 }
 
-impl AsVector<char> for String {
-    fn as_vec(&self) -> Vec<char> {
-        self.chars().collect()
-    }
-}
-
-impl<K> AsVector<K> for &[K]
+impl<K> AsSlice<K> for &[K]
 where
     K: Copy,
 {
-    fn as_vec(&self) -> Vec<K> {
-        self.to_vec()
+    fn as_slice(&self) -> &[K] {
+        self
+    }
+}
+
+impl<K> AsSlice<K> for Vec<K> {
+    fn as_slice(&self) -> &[K] {
+        self.as_slice()
     }
 }
 
@@ -79,7 +79,7 @@ where
     }
 
     pub fn value(&self) -> Option<&V> {
-        self.get(&[])
+        self.get(&[] as &[K])
     }
 
     // We could cache the number of elements at the root so this is O(1) but on the other hand,
@@ -140,7 +140,11 @@ where
         Box::new(self.iter_fast().map(|(_, value)| value))
     }
 
-    pub fn values_with<'a>(&'a self, key: &[K]) -> Box<dyn Iterator<Item = &'a V> + 'a> {
+    pub fn values_with<'a, T>(&'a self, key: T) -> Box<dyn Iterator<Item = &'a V> + 'a>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return self.values();
         }
@@ -155,7 +159,11 @@ where
     /// All the keys matching the given prefix and their values. Keys do not include the prefix for
     /// performance reasons. The caller can reconstruct the full keys by prepending the prefix they
     /// passed as a parameter if needed.
-    pub fn iter_with<'a>(&'a self, key: &[K]) -> Box<dyn Iterator<Item = (Vec<K>, &'a V)> + 'a> {
+    pub fn iter_with<'a, T>(&'a self, key: T) -> Box<dyn Iterator<Item = (Vec<K>, &'a V)> + 'a>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return self.iter();
         }
@@ -168,7 +176,11 @@ where
     }
 
     // Internal method returning the node for a given key. Used for testing and to implement get().
-    fn lookup(&self, key: &[K]) -> Option<&RadixTree<K, V>> {
+    fn lookup<T>(&self, key: T) -> Option<&RadixTree<K, V>>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return Some(self);
         }
@@ -181,7 +193,11 @@ where
     }
 
     // Hate this duplication but haven't yet found a way to avoid it.
-    fn lookup_mut(&mut self, key: &[K]) -> Option<&mut RadixTree<K, V>> {
+    fn lookup_mut<T>(&mut self, key: T) -> Option<&mut RadixTree<K, V>>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return Some(self);
         }
@@ -194,15 +210,26 @@ where
     }
 
     // Writing this in terms of lookup() hopefully doesn't hurt performance.
-    pub fn get(&self, key: &[K]) -> Option<&V> {
+    pub fn get<T>(&self, key: T) -> Option<&V>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         self.lookup(key).and_then(|node| node.value.as_ref())
     }
 
-    pub fn get_mut(&mut self, key: &[K]) -> Option<&mut V> {
+    pub fn get_mut<T>(&mut self, key: T) -> Option<&mut V>
+    where
+        T: AsSlice<K>,
+    {
         self.lookup_mut(key).and_then(|node| node.value.as_mut())
     }
 
-    pub fn insert(&mut self, key: &[K], value: V) -> Option<V> {
+    pub fn insert<T>(&mut self, key: T, value: V) -> Option<V>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return self.value.replace(value);
         }
@@ -229,7 +256,11 @@ where
         None
     }
 
-    pub fn remove(&mut self, key: &[K]) -> Option<V> {
+    pub fn remove<T>(&mut self, key: T) -> Option<V>
+    where
+        T: AsSlice<K>,
+    {
+        let key = key.as_slice();
         if key.is_empty() {
             return self.value.take();
         }
@@ -299,20 +330,20 @@ mod tests {
     fn radix_tree_works() {
         let mut tree = RadixTree::new();
         assert_eq!(tree.value, None);
-        assert_eq!(tree.insert(b"foo", 42), None);
+        assert_eq!(tree.insert("foo", 42), None);
         assert_eq!(tree.value, None);
         assert_eq!(tree.edges.len(), 1);
-        let _node = tree.lookup(b"foo");
+        let _node = tree.lookup("foo");
         assert_eq!(_node.and_then(|node| node.value), Some(42));
         assert!(_node.is_some_and(|node| node.edges.is_empty()));
 
-        assert!(tree.insert(b"bar", 13).is_none());
-        assert_eq!(tree.get(b"bar"), Some(&13));
-        assert!(tree.insert(b"baz", 7).is_none());
-        assert_eq!(tree.get(b"baz"), Some(&7));
+        assert!(tree.insert("bar", 13).is_none());
+        assert_eq!(tree.get("bar"), Some(&13));
+        assert!(tree.insert("baz", 7).is_none());
+        assert_eq!(tree.get("baz"), Some(&7));
         // This should have split the "bar" node into a "ba" node with one "r" edge and one "z"
         // edge with the values of "bar" and "baz" respectively.
-        let _node = tree.lookup(b"ba");
+        let _node = tree.lookup("ba");
         assert!(_node.is_some_and(|node| node.value.is_none()));
         assert!(_node.is_some_and(|node| node.edges.len() == 2));
         assert!(_node.is_some_and(|node| {
@@ -324,17 +355,17 @@ mod tests {
             prefix == b"z" && child.value == Some(7)
         }));
 
-        assert_eq!(tree.insert(b"ba", 18), None);
-        assert_eq!(tree.get(b"ba"), Some(&18));
-        assert_eq!(tree.insert(b"barbie", 23), None);
-        assert_eq!(tree.get(b"barbie"), Some(&23));
-        assert_eq!(tree.get(b"bag"), None);
-        assert_eq!(tree.get(b"qux"), None);
-        assert_eq!(tree.insert(b"ba", 27), Some(18));
-        assert_eq!(tree.get(b"ba"), Some(&27));
+        assert_eq!(tree.insert("ba", 18), None);
+        assert_eq!(tree.get("ba"), Some(&18));
+        assert_eq!(tree.insert("barbie", 23), None);
+        assert_eq!(tree.get("barbie"), Some(&23));
+        assert_eq!(tree.get("bag"), None);
+        assert_eq!(tree.get("qux"), None);
+        assert_eq!(tree.insert("ba", 27), Some(18));
+        assert_eq!(tree.get("ba"), Some(&27));
 
         println!("Keys matching prefix \"ba\" and their values");
-        for kv in tree.iter_with(b"ba") {
+        for kv in tree.iter_with("ba") {
             println!("{kv:?}");
         }
 
@@ -348,11 +379,11 @@ mod tests {
             println!("{kv:?}");
         }
 
-        assert_eq!(tree.remove(b"bar"), Some(13));
+        assert_eq!(tree.remove("bar"), Some(13));
         println!("{tree:?}");
         // XXX Check that we now have "ba" -> "rbie" and "ba" -> "z"
-        assert_eq!(tree.get(b"bar"), None);
-        assert_eq!(tree.remove(b"baz"), Some(7));
-        assert_eq!(tree.remove(b"baz"), None);
+        assert_eq!(tree.get("bar"), None);
+        assert_eq!(tree.remove("baz"), Some(7));
+        assert_eq!(tree.remove("baz"), None);
     }
 }
